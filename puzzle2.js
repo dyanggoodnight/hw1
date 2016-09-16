724387618eedc5380aae78ed70b222ed383a9761
dyanggoodnight <dyang@cca.edu>
2016-09-15 20:19:55 -0700

    Create puzzle2.js

---
+++ puzzle2.js
@@ -0,0 +1,13 @@
+function oneStep(n,direction){
+  var steps = 0;
+  while (steps < n){
+    direction();
+    steps = steps +1;
+  }
+}
+  
+oneStep(6,down);
+oneStep(3,right);
+oneStep(2,up);
+oneStep(2,right);
+oneStep(3,down);
